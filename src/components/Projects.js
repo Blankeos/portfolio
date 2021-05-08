0d42deb94d958db6b839391384aedac8a130dfc8
Blankeos <carloantonioct@gmail.com>
2021-05-09 02:24:51 +0800

    Updated Cafely details

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -17,6 +17,16 @@ const Projects = () => {
 
         <div className="mt-10 mx-auto max-w-xl flex flex-col gap-5 justify-items-center">
           <ProjectCard
+            link="https://cafely.vercel.app/login"
+            label="Cafe.ly"
+            description="A product review website for coffee for my Software Engineering class. It's still in progress but it has working Firebase auth and serverless functions communicating with a Mongo database."
+            buttonLabel="Visit cafely.vercel.app"
+            imgurl="https://i.ibb.co/MgKwDMz/image.png"
+            icon={<BsArrowRight size="1.2em" />}
+            githubLink="https://github.com/Blankeos/cit214-productreviewapp"
+            btnClass="bg-yellow-500 text-white"
+          />
+          <ProjectCard
             link="https://play.google.com/store/apps/details?id=com.DigikattStudios.Chromeleon"
             label="Chromeleon"
             description="A Unity game I published on Google Play back in 2016."
@@ -32,16 +42,6 @@ const Projects = () => {
             buttonLabel="Go to Digikatt Studios"
             imgurl="https://raw.githubusercontent.com/Blankeos/portfolio/master/src/imgs/axelanimated.gif"
             btnClass="bg-red-600 text-white"
-          />
-          <ProjectCard
-            link="https://cafely-vercel-test-blankeos.vercel.app/login"
-            label="Cafe.ly"
-            description="A product review website for coffee for my Software Engineering class. It's still in progress but it has working auth with Firebase."
-            buttonLabel="Visit Site"
-            imgurl="https://i.ibb.co/MgKwDMz/image.png"
-            icon={<BsArrowRight size="1.2em" />}
-            githubLink="https://github.com/Blankeos/cit214-productreviewapp"
-            btnClass="bg-yellow-500 text-white"
           />
           <ProjectCard
             link="https://blankeos.github.io/react-pokemon-search/#/"
